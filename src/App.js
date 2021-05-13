edcc91ec1fb0f4844886e8e8a309bfca4c325ac3
sharath <sharathmanikanta@gmail.com>
2021-05-13 13:14:00 +0530

    Added collecition preview and collection item component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,17 @@ import React from 'react';
 import {Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './components/shop/shop.component';
 
-const HatsPage = () =>(
-<div>
-  <h1>Hats page</h1>
-</div>
 
-)
+
 
 function App() {
   return (
     <div>
       <Switch>
     <Route exact path='/' component={HomePage}></Route>
-    <Route exact path='/Hats' component={HatsPage}></Route>
+    <Route exact path='/Shop' component={ShopPage}></Route>
     </Switch>
     </div>
    // <div><HomePage></HomePage></div>
